d84b340391928fc6dc8b3a1e06c9862b0c5fc126
Eka666Sed <cat.sedoff@gmail.com>
2025-01-02 21:01:39 +0100

    First commit_add custom view

---
+++ app/src/main/java/com/yandex/practicum/sample/customviews/views/ColorChangeButton.kt
@@ -0,0 +1,13 @@
+package com.yandex.practicum.sample.customviews.views
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.core.content.ContextCompat
+import androidx.appcompat.widget.AppCompatButton
+
+class ColorChangeButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatButton(context, attrs, defStyleAttr) {
+}
---
+++ app/src/main/java/com/yandex/practicum/sample/customviews/views/CustomFrameLayout.kt
@@ -0,0 +1,12 @@
+package com.yandex.practicum.sample.customviews.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class CustomFrameLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+}
