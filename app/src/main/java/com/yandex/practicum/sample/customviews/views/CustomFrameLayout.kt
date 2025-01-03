37660c4fd1ba49f1a7ea05db2f5dd8c000062ea8
Eka666Sed <cat.sedoff@gmail.com>
2025-01-03 14:17:26 +0100

    add compound views

--- app/src/main/java/com/yandex/practicum/sample/customviews/views/CustomFrameLayout.kt
+++ app/src/main/java/com/yandex/practicum/sample/customviews/views/CustomFrameLayout.kt
@@ -2,11 +2,23 @@ package com.yandex.practicum.sample.customviews.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.FrameLayout
+import com.yandex.practicum.sample.customviews.R
 
 class CustomFrameLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
+
+    var circleView: PracticumCircleView
+    var button: ColorChangeButton
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.view_custom_compound, this, true)
+
+        circleView = findViewById(R.id.circle_view)
+        button = findViewById(R.id.button)
+    }
 }
